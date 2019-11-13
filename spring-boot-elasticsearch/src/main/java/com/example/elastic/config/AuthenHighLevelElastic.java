e362fe39478b43328e3aae74004a63bd1024f08d
lsd <viviandavid@126.com>
2019-11-13 14:10:37 +0800

    修改配置文件

--- spring-boot-elasticsearch/src/main/java/com/example/elastic/config/AuthenHighLevelElastic.java
+++ spring-boot-elasticsearch/src/main/java/com/example/elastic/config/AuthenHighLevelElastic.java
@@ -9,20 +9,33 @@ import org.apache.http.impl.nio.client.HttpAsyncClientBuilder;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestClientBuilder;
 import org.elasticsearch.client.RestHighLevelClient;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AuthenHighLevelElastic {
 
+    @Value("${spring.data.elasticsearch.cluster-nodes}")
+    private String hostname;
+
+    @Value("${spring.data.elasticsearch.cluster-port}")
+    private int port;
+
+    @Value("${spring.data.elasticsearch.username}")
+    private String userName;
+
+    @Value("${spring.data.elasticsearch.password}")
+    private String password;
+
     @Bean
     public RestHighLevelClient client(){
         /*用户认证对象*/
         final CredentialsProvider credentialsProvider = new BasicCredentialsProvider();
         /*设置账号密码*/
-        credentialsProvider.setCredentials(AuthScope.ANY,new UsernamePasswordCredentials("elastic", "654321"));
+        credentialsProvider.setCredentials(AuthScope.ANY,new UsernamePasswordCredentials(userName, password));
         /*创建rest client对象*/
-        RestClientBuilder builder = RestClient.builder(new HttpHost("10.20.4.66",9200))
+        RestClientBuilder builder = RestClient.builder(new HttpHost(hostname,port))
                 .setHttpClientConfigCallback(new RestClientBuilder.HttpClientConfigCallback() {
                     @Override
                     public HttpAsyncClientBuilder customizeHttpClient(HttpAsyncClientBuilder httpAsyncClientBuilder) {
